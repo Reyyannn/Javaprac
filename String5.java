eb75a95cc601f54e40e6d46fa572479329b6486c
Reyyannn <aastha.acharya08@gmail.com>
2023-02-07 22:39:26 -0600

    added

---
+++ String5.java
@@ -0,0 +1,108 @@
+package prac;
+
+public class String5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String salad= "chicken salad";
+		int a= salad.indexOf('k', 4);
+		System.out.println(a);
+		
+		int count= 0;
+		for(int i= 0 ; i <salad.length();i++) {
+		if (salad.charAt(i)=='k') {
+			System.out.println(i);
+			count= count +1 ;
+		}
+		
+		
+	}
+		System.out.println(count);
+
+		//array
+		
+		int nums [] = new int[4];
+		nums[0]= 20;
+		nums[1]= 200;
+		nums[2]=30;
+		nums[3]= 300;
+		
+		System.out.println(nums [0]);
+		System.out.println(nums [1]);
+		System.out.println(nums [2]);
+		System.out.println(nums [3]);
+		
+		String s= "salad";
+		for(int i =0; i < s.length(); i++) {
+			//System.out.println(i);
+			System.out.println(s.charAt(i));
+		}	
+		
+		//program 3
+		int numss []= new int [3];
+		numss[0]= 400;
+		numss[1]= 500;
+		nums[2]= 600;
+		
+		System.out.println(numss.length);
+		for(int i= 0; i< numss.length;i++) {
+			System.out.println(numss[i]);
+		}
+		//program4
+		
+		String cities []= new String [4];
+		cities [0]= "nagarkot";
+		cities [1]= "balambhu";
+		cities [2]= "dharan";
+		cities [3]= "austin";
+		
+		for(int i=0; i<cities.length;i++) {
+			System.out.println(cities[i]);
+		}
+		
+		//program5
+		//0  1  2  3  4  5
+		int numberc []= {12,13,14,15,16,18};
+		for(int i=0; i<numberc.length;i++) {
+			System.out.println(numberc[i]);
+		}
+		
+		//program 6
+		String foods []= {"bhaat", "daal", "tarkari", "masu"};
+		for(int i=0; i<foods.length;i++) {
+			System.out.println(foods[i]);
+		}
+	// program 7
+		char cb []= {'a','a','n','d','a'};
+		for(int i=0; i<cb.length;i++) {
+			System.out.println(i);
+			System.out.println(cb[i]);
+		}
+		//program 8
+		String firstName= "aastha";
+		char [] ac= firstName.toCharArray();
+		for(int i=0; i<ac.length;i++) {
+			//System.out.println(i);
+			System.out.println(ac[i]);
+		}
+		//program 9
+		String e = "learning javascript";
+		String h2 []= e.split(" ");
+		String h3 [] = e.split("r");
+		
+		for(int i=0; i<h2.length;i++) {
+			System.out.println(i);
+			System.out.println(h2[i]);
+			
+			for(int i1= 0; i1<h3.length;i1++) {
+				//System.out.println(i1);
+				System.out.println(h3[i1]);
+			}
+		}
+		
+		
+		
+		
+}
+}
